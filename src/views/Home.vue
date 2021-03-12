bab55f80549a336d965fcb326a7c3bc0544b7142
AmelieChevalier <ameliechevalier3006@gmail.com>
2021-03-12 11:26:23 +0100

    add json data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,20 +2,41 @@
   <div class="home">
     <img alt="Logo" src="../assets/logo.png" width="140">
     <HelloWorld msg="Welcome to your app"/>
+    <h1>Bienvenue sur {{appInfo.name}}</h1>
   </div>
 </template>
 
 <script lang="ts">
+import axios from 'axios';
 import Vue from "vue"
 import HelloWorld from '@/components/HelloWorld.vue'
 import Auth from "../service/Auth"
 
 export default Vue.extend({
-    name: "Home",
-    components: { HelloWorld },
-    mounted()
-    {
-        console.log(Auth.getApiKey())
+  name: "Home",
+  components: { HelloWorld },
+  data()
+  {
+    return {
+      appInfo: {},
+      appSection: {}
     }
+  },
+  mounted()
+  {
+    console.log(Auth.getApiKey())
+    this.loadData()
+    .then((result: any) => {
+      this.appInfo = result.appData
+      this.appSection = result.tree
+    })
+  },
+  methods:
+  {
+    async loadData() {
+      const data = await axios.get(`${window.location.origin + window.location.pathname}/data.json`);
+      return data.data
+    }
+  }
 })
 </script>
